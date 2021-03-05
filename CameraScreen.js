de3387c80edd96e299083d93d6d35347b7523301
Eflamm <eflamm.ollivier@gmail.com>
2021-03-05 23:59:45 +0100

    save product with name

--- CameraScreen.js
+++ CameraScreen.js
@@ -30,7 +30,7 @@ export default function CameraScreen({navigation}) {
     return <Text>No access to the camera </Text>;
   }
 
-  const takePicture = async () => {
+  const onClickTakePicture = async () => {
     if(camera) {
       if(!photoList.length) {
         setCreationDate(new Date());
@@ -62,12 +62,11 @@ export default function CameraScreen({navigation}) {
       setPhotoList([]);
 
       // save the product to the database
-      const product = {event: "someEvent", meal: "breakfast", name: "noodles"};
+      const product = {event: "someEvent", meal: "breakfast", name: productName};
       EventService.addProduct(product, creationDate);
 
     }
   }
-
 
   const cancelTakingPictures = async () => {
     // TODO
@@ -91,25 +90,30 @@ export default function CameraScreen({navigation}) {
     // TODO open the keyboard
   }
 
+  const onClickValidate = () => {
+    setModalVisible(false);
+    savePictures();
+  };
+
   const cancelSavingProduct = () => {
     setProductName("");
     setModalVisible(false);
   }
-
-  const flushPictures = () => {
-    // TODO remove all pictures from cache
-    setPhotoList([]);
-  }
-
+  
   const displayPreviewPhotos = () => {
     return photoList.map(photo => {
-      return <TouchableOpacity onPress={() => removePhoto(photo.id)} key={photo.id}>
+      return <TouchableOpacity onPress={() => onClickPhotoBubble(photo.id)} key={photo.id}>
               <Image style={styles.photoPreview} source={{uri: photo.uri}}/>
             </TouchableOpacity>
     })
   }
+  
+  const onClickFlushPictures = () => {
+    // TODO remove all pictures from cache
+    setPhotoList([]);
+  }
 
-  const removePhoto = (id) => {
+  const onClickPhotoBubble = (id) => {
     setPhotoList(photoList.filter(photo => photo.id !== id));
     displayPreviewPhotos();
   }
@@ -124,7 +128,7 @@ export default function CameraScreen({navigation}) {
                 <Path fill="none" stroke="lightgray" stroke-width="1.06" d="M16,4 L4,16"></Path>
             </Svg>
           </TouchableOpacity>
-          <TouchableOpacity style={styles.takePictureButton} onPress={() => takePicture()}>
+          <TouchableOpacity style={styles.takePictureButton} onPress={() => onClickTakePicture()}>
             <Svg height="40%" width="40%" viewBox="0 0 98 98" >
               <Circle cx="50" cy="50" r="45" stroke="darkgrey" strokeWidth="5" fill="lightgray" />
             </Svg>
@@ -140,7 +144,7 @@ export default function CameraScreen({navigation}) {
         {displayPreviewPhotos()}
         {
           photoList.length ? 
-          <TouchableOpacity style={styles.flushPicturesButton} onPress={() => flushPictures()}>
+          <TouchableOpacity style={styles.flushPicturesButton} onPress={() => onClickFlushPictures()}>
             <Svg width="50" height="50" viewBox="0 0 256 256"  fill="lightgray">
               <Path d="M183.191,174.141c2.5,2.498,2.5,6.552,0,9.05c-1.249,1.25-2.889,1.875-4.525,1.875c-1.638,0-3.277-0.625-4.525-1.875  l-46.142-46.142L81.856,183.19c-1.249,1.25-2.888,1.875-4.525,1.875c-1.638,0-3.277-0.625-4.525-1.875c-2.5-2.498-2.5-6.552,0-9.05  l46.143-46.143L72.806,81.856c-2.5-2.499-2.5-6.552,0-9.05c2.497-2.5,6.553-2.5,9.05,0l46.142,46.142l46.142-46.142  c2.497-2.5,6.553-2.5,9.051,0c2.5,2.499,2.5,6.552,0,9.05l-46.143,46.142L183.191,174.141z M256,128C256,57.42,198.58,0,128,0  C57.42,0,0,57.42,0,128c0,70.58,57.42,128,128,128C198.58,256,256,198.58,256,128z M243.2,128c0,63.521-51.679,115.2-115.2,115.2  c-63.522,0-115.2-51.679-115.2-115.2C12.8,64.478,64.478,12.8,128,12.8C191.521,12.8,243.2,64.478,243.2,128z"/>
             </Svg>
@@ -172,7 +176,7 @@ export default function CameraScreen({navigation}) {
             </TouchableOpacity>
             <TouchableOpacity
               style={[styles.modalButton, styles.modalValidateButton]}
-              onPress={() => setModalVisible(!isModalVisible)}
+              onPress={() => onClickValidate()}
             >
               <Text style={styles.modalButtonText}>Validate</Text>
             </TouchableOpacity>
